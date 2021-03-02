d97704eeb8e9be4457a5a3bcb79fe17491971784
Yichen Wu <yichen.wu3@mail.mcgill.ca>
2021-03-02 00:42:03 -0500

    corrected the controller

--- RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/controller/RepairShopRestController.java
+++ RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/controller/RepairShopRestController.java
@@ -4,15 +4,12 @@ import ca.mcgill.ecse321.repairshop.service.RepairShopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
 public class RepairShopRestController {
+    @Autowired
+    private RepairShopService service;
 
 
-    @CrossOrigin(origins = "*")
-    @RestController
-    public class EventRegistrationRestController {
-
-        @Autowired
-        private RepairShopService service;
-
-    }
 }
