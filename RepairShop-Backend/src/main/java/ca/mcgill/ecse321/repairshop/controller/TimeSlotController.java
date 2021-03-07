6147236fd516c437d6cd04b24c6aced3db453240
florenceyared <florence.yared@mail.mcgill.ca>
2021-03-06 23:45:52 -0500

    some methods

--- RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/controller/TimeSlotController.java
+++ RepairShop-Backend/src/main/java/ca/mcgill/ecse321/repairshop/controller/TimeSlotController.java
@@ -6,4 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 @RestController
 public class TimeSlotController {
+
+    @PostMapping(value = {"/timeSlot", "/timeSlot/"})
+    public TimeSlotDto createTimeSlot (@RequestParam Date date, @RequestParam @DateTimeFormat(iso  = DateTimeFormat.ISO.TIME, pattern = "HH:mm") LocalTime startTime, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME, pattern = "HH:mm") LocalTime endTime) throws IllegalArgumentException {
+        TimeSlot timeSlot = TimeSlotService.createTimeSlot(date, Time.valueOf(startTime), Time.ValueOf(endTime));
+        return convertToDto(timeSlot);
+    }
+
 }
