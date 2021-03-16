b1db015a1a4493b752d1105b0475885b5412dcaf
himelsaha29 <himelsaha29@live.com>
2021-03-16 03:15:49 -0400

    Test class initiated. First case passes.

--- RepairShop-Backend/src/test/java/ca/mcgill/ecse321/repairshop/service/TestRepairShopService.java
+++ RepairShop-Backend/src/test/java/ca/mcgill/ecse321/repairshop/service/TestRepairShopService.java
@@ -1,4 +1,67 @@
 package ca.mcgill.ecse321.repairshop.service;
 
+import ca.mcgill.ecse321.repairshop.dao.ServiceRepository;
+import ca.mcgill.ecse321.repairshop.model.BookableService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.lenient;
+
+@ExtendWith(MockitoExtension.class)
 public class TestRepairShopService {
+    @Mock
+    private ServiceRepository serviceRepository;
+
+    @InjectMocks
+    private RepairShopService repairShopService;
+
+    private static String NAME = "repairshop";
+    private static float COST = 21.3f;
+    private static int DURATION = 10;
+
+    @BeforeEach
+    public void setMockOutPut(){
+        lenient().when(serviceRepository.findById(anyLong())).thenAnswer((InvocationOnMock invocation) -> {
+            if(invocation.getArgument(0).equals(NAME)){
+                BookableService bookableService = new BookableService();
+                bookableService.setName(NAME);
+                bookableService.setCost(COST);
+                bookableService.setDuration(DURATION);
+                return Optional.of(bookableService);
+            }else{
+                return Optional.empty();
+            }
+        });
+
+        Answer<?> returnParameterAsAnswer = (InvocationOnMock invocation) -> {
+            return invocation.getArgument(0);
+        };
+        lenient().when(serviceRepository.save(any(BookableService.class))).thenAnswer(returnParameterAsAnswer);
+    }
+
+    @Test
+    public void testCreateServiceSuccessfully(){
+        BookableService bookableService = new BookableService();
+        bookableService.setName(NAME);
+        bookableService.setCost(COST);
+        bookableService.setDuration(DURATION);
+        BookableService createdService = null;
+        createdService = repairShopService.createService(NAME, COST, DURATION);
+
+
+        assertNotNull(createdService);
+    }
 }
