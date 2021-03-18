8fe9b4250922ba505d4cd7a128f053811ce996fc
Rakshitha Ravi <rakshitharavi18@hotmail.com>
2021-03-17 22:33:24 -0400

    Update TestPersonService.java

--- RepairShop-Backend/src/test/java/ca/mcgill/ecse321/repairshop/service/TestPersonService.java
+++ RepairShop-Backend/src/test/java/ca/mcgill/ecse321/repairshop/service/TestPersonService.java
@@ -1,8 +1,12 @@
 package ca.mcgill.ecse321.repairshop.service;
 
 import ca.mcgill.ecse321.repairshop.dao.*;
+import ca.mcgill.ecse321.repairshop.dto.BusinessDto;
+import ca.mcgill.ecse321.repairshop.dto.CustomerDto;
 import ca.mcgill.ecse321.repairshop.model.Administrator;
+import ca.mcgill.ecse321.repairshop.model.Business;
 import ca.mcgill.ecse321.repairshop.model.Customer;
+import ca.mcgill.ecse321.repairshop.utility.BusinessException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.invocation.InvocationOnMock;
@@ -52,58 +56,103 @@ public class TestPersonService {
     public void setMockOutput() {
 
         // customer by email
-        lenient().when(customerRepository.findCustomerByEmail(anyString())).thenAnswer( (InvocationOnMock invocation) -> {
-            if(invocation.getArgument(0).equals(CUSTOMER_EMAIL)) {
+        lenient().when(customerRepository.findCustomerByEmail(anyString())).thenAnswer((InvocationOnMock invocation) -> {
+            if (invocation.getArgument(0).equals(CUSTOMER_EMAIL)) {
                 Customer customer = new Customer();
                 customer.setEmail(CUSTOMER_EMAIL);
                 return customer;
-            }
-            else{
+            } else {
                 return null;
             }
         });
 
         //admin by email
 
-        lenient().when(administratorRepository.findAdministratorByEmail(anyString())).thenAnswer( (InvocationOnMock invocation) -> {
-            if(invocation.getArgument(0).equals(ADMIN_EMAIL)) {
+        lenient().when(administratorRepository.findAdministratorByEmail(anyString())).thenAnswer((InvocationOnMock invocation) -> {
+            if (invocation.getArgument(0).equals(ADMIN_EMAIL)) {
                 Administrator administrator = new Administrator();
                 administrator.setEmail(ADMIN_EMAIL);
                 return administrator;
-            }
-            else{
+            } else {
                 return null;
             }
         });
 
         //technician by email
-        lenient().when(technicianRepository.findTechnicianByEmail(anyString())).thenAnswer( (InvocationOnMock invocation) -> {
-            if(invocation.getArgument(0).equals(TECH_EMAIL)) {
+        lenient().when(technicianRepository.findTechnicianByEmail(anyString())).thenAnswer((InvocationOnMock invocation) -> {
+            if (invocation.getArgument(0).equals(TECH_EMAIL)) {
                 Administrator administrator = new Administrator();
                 administrator.setEmail(TECH_EMAIL);
                 return administrator;
-            }
-            else{
+            } else {
                 return null;
             }
         });
 
 
         //owner by email
-        lenient().when(ownerRepository.findOwnerByEmail(anyString())).thenAnswer( (InvocationOnMock invocation) -> {
-            if(invocation.getArgument(0).equals(OWNER_EMAIL)) {
+        lenient().when(ownerRepository.findOwnerByEmail(anyString())).thenAnswer((InvocationOnMock invocation) -> {
+            if (invocation.getArgument(0).equals(OWNER_EMAIL)) {
                 Administrator administrator = new Administrator();
                 administrator.setEmail(OWNER_EMAIL);
                 return administrator;
-            }
-            else{
+            } else {
                 return null;
             }
         });
 
 
-            }
+    }
+/*
+    @Test
+    public void testCreateCustomer() {
+        CustomerDto customerDto = new CustomerDto();
+        customerDto.setEmail(CUSTOMER_EMAIL);
 
- 
+        Customer createdCustomer = null;
+        try {
+            createdCustomer = personService.createCustomer();
+        } catch (BusinessException e) {
+            e.printStackTrace();
+            fail();
+        }
+        assertNotNull(createdCustomer);
+        assertEquals(createdCustomer.getEmail(), customerDto.getEmail());
+        }
+
+          */
+
+
+
+    @Test
+    public void testDeleteCustomer(){
 
     }
+
+    @Test
+    public void testDeleteOwner(){
+
+    }
+
+    @Test
+    public void testDeleteAdmin(){
+
+    }
+
+
+    @Test
+    public void testDeleteTechnician(){
+
+    }
+
+
+    @Test
+    public void testGetcustomerWithInvalidEmail() {
+
+
+    }
+
+
+
+    }
+
