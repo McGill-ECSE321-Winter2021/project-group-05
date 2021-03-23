a3f5a58f97f3156ee072b4d0cf16264e8ebf8a4f
Saikou Ceesay <ceesaysaikou72@gmail.com>
2021-03-23 13:32:33 -0400

    fix compilation error due to missing image

--- RepairShop-Frontend/src/App.vue
+++ RepairShop-Frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/repairShop.png">
+    <img width="100" height="100" src="./assets/repairShop.png">
     <router-view></router-view>
   </div>
 </template>
@@ -18,6 +18,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
