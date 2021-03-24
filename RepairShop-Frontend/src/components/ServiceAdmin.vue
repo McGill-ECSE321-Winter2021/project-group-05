21546dae9be68ab5edfc45d341d733ef6b887866
himelsaha29 <himelsaha29@live.com>
2021-03-24 19:45:57 -0400

    Create service (for Admin) Vue component created

---
+++ RepairShop-Frontend/src/components/ServiceAdmin.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="serviceAdmin">
+    <h2>RepairShop</h2>
+    <label align=CENTER>Create new service</label>
+    <label align=CENTER> </label>
+    <table align="CENTER">
+      <tr>
+          <td>
+              <input type="text" placeholder="Enter name">
+          </td>
+          <td>
+              <input type="number" step="0.01" placeholder="Enter cost">
+          </td>
+          <td>
+              <input type="number" placeholder="Enter duration">
+          </td>
+      </tr>
+    </table>
+    <Button align=CENTER>Create</Button>
+    <p>
+      <span style="color:red">Error: Message text comes here</span>
+    </p>
+  </div>
+</template>
+
+<script>
+</script>
+
+
+<style>
+  #serviceAdmin {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    background: #f2ece8;
+  }
+</style>
--- RepairShop-Frontend/src/router/index.js
+++ RepairShop-Frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import ServiceCustomer from '@/components/ServiceCustomer'
+import ServiceAdmin from '@/components/ServiceAdmin'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'ServiceCustomer',
       component: ServiceCustomer
+    },
+    {
+      path: '/createService',
+      name: 'ServiceAdmin',
+      component: ServiceAdmin
     }
   ]
 })
