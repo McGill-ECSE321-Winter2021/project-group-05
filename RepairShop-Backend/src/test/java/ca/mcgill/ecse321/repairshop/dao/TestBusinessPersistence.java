7b0f7f06efd4b5500d308f078118f3f2acfc124d
Yichen Wu <yichen.wu3@mail.mcgill.ca>
2021-03-28 16:45:51 -0400

    deli2 fix: seperate the DAO tests

---
+++ RepairShop-Backend/src/test/java/ca/mcgill/ecse321/repairshop/dao/TestBusinessPersistence.java
@@ -0,0 +1,60 @@
+package ca.mcgill.ecse321.repairshop.dao;
+
+import ca.mcgill.ecse321.repairshop.model.Business;
+import ca.mcgill.ecse321.repairshop.model.RepairShop;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class TestBusinessPersistence {
+    @Autowired
+    private BusinessRepository businessRepository;
+
+    @AfterEach
+    public void clearDatabase() {
+        // First, we clear business to avoid exceptions due to inconsistencies
+        businessRepository.deleteAll();
+
+    }
+    /**
+     * Testing persisting and loading business
+     */
+    public void testPersistAndLoadBusiness(){
+        String address = "MTL";
+        String email = "business@123.com";
+        String phone = "514123456";
+        String name = "testBusiness";
+        Business business = new Business();
+
+        business.setAddress(address);
+        business.setEmail(email);
+        business.setPhoneNumber(phone);
+        business.setName(name);
+
+        RepairShop rs = new RepairShop();
+        business.setRepairShop(rs);
+
+        businessRepository.save(business);
+
+        Long id = business.getId();
+
+        business = businessRepository.findBusinessById(id);
+
+        assertNotNull(business);
+        assertEquals(address,business.getAddress());
+        assertEquals(email,business.getEmail());
+        assertEquals(id,business.getId());
+        assertEquals(phone,business.getPhoneNumber());
+        assertEquals(name,business.getName());
+        assertNotNull(business.getRepairShop());
+
+    }
+}
+
---
+++ RepairShop-Backend/src/test/java/ca/mcgill/ecse321/repairshop/dao/TestTimeSlotPersistence.java
@@ -0,0 +1,64 @@
+package ca.mcgill.ecse321.repairshop.dao;
+
+import ca.mcgill.ecse321.repairshop.model.RepairShop;
+import ca.mcgill.ecse321.repairshop.model.TimeSlot;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class TestTimeSlotPersistence {
+
+
+    @Autowired
+    private TimeSlotRepository timeSlotRepository;
+
+    @AfterEach
+    public void clearDatabase() {
+
+        timeSlotRepository.deleteAll();
+    }
+
+    /**
+     * Testing persisting and loading timeslot
+     */
+    public void testPersistAndLoadTimeSlot(){
+
+        Date date = java.sql.Date.valueOf(LocalDate.of(2020, Month.JANUARY, 31));
+        Time startTime = java.sql.Time.valueOf(LocalTime.of(11, 35));
+        Time endTime = java.sql.Time.valueOf(LocalTime.of(13, 25));
+
+        TimeSlot timeSlot = new TimeSlot();
+        timeSlot.setEndTime(endTime);
+        timeSlot.setStartTime(startTime);
+        timeSlot.setDate(date);
+
+        RepairShop rs = new RepairShop();
+        timeSlot.setRepairShop(rs);
+
+        timeSlotRepository.save(timeSlot);
+
+        Long timeSlotID =timeSlot.getId();
+
+        timeSlot = timeSlotRepository.findTimeSlotById(timeSlotID);
+
+        assertNotNull(timeSlot);
+        assertEquals(timeSlotID, timeSlot.getId());
+        assertEquals(date.toString(), timeSlot.getDate().toString());
+        assertEquals(startTime.toString(), timeSlot.getStartTime().toString());
+        assertEquals(endTime.toString(), timeSlot.getEndTime().toString());
+        assertNotNull(timeSlot.getRepairShop());
+    }
+}
