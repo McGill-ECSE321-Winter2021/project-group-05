25a3fc4da90b9c54440e9572e26bb4752c62495c
Saikou Ceesay <ceesaysaikou72@gmail.com>
2021-03-28 13:15:49 -0400

    creating services for the system implemented [by himel [IN PROGRESS]]

---
+++ RepairShop-Frontend/src/components/createServiceAdmin.js
@@ -0,0 +1,76 @@
+import axios from "axios";
+import AdminHeader from "./AdminHeader";
+
+var config = require("../../config");
+
+var frontendUrl = "http://" + config.dev.host + ":" + config.dev.port;
+var backendUrl =
+  "http://" + config.dev.backendHost + ":" + config.dev.backendPort;
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { "Access-Control-Allow-Origin": frontendUrl }
+});
+
+function BookableServiceDto(name, cost, duration) {
+  this.name = name;
+  this.cost = cost;
+  this.duration = duration;
+}
+
+export default {
+  name: "createServiceAdmin",
+  components: {
+    AdminHeader
+  },
+  data() {
+    return {
+      services: [],
+      bookableService: {
+        newService: "",
+        newCost: "",
+        newDuration: ""
+      },
+      errorCreateService: "",
+      response: []
+    };
+  },
+
+  created: function() {
+    // new added
+    // Initializing services from backend
+    AXIOS.get("/bookableServices")
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.services = response.data;
+      })
+      .catch(e => {
+        this.errorCreateService = e;
+      });
+  },
+
+  methods: {
+    createServiceAdmin: function(serviceName, serviceCost, serviceDuration) {
+      console.log(serviceName, serviceCost, serviceDuration);
+      const bookableServiceDto = new BookableServiceDto(
+        serviceName,
+        serviceCost,
+        serviceDuration
+      );
+      AXIOS.post("/bookableService", bookableServiceDto)
+        .then(response => {
+          // JSON responses are automatically parsed.
+          this.services.push(response.data);
+          this.newService = "";
+          this.newCost = "";
+          this.newDuration = "";
+          this.errorCreateService = "";
+        })
+        .catch(e => {
+          var errorMsg = e.response.data.message;
+          console.log(errorMsg);
+          this.errorCreateService = errorMsg;
+        });
+    }
+  }
+};
