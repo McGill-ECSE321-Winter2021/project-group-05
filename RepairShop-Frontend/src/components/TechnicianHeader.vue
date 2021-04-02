337670e2806ce31a5c089f24a26a121ed7d9edc4
himelsaha29 <himelsaha29@live.com>
2021-04-02 12:25:16 -0400

    Technician header tabs fixed

--- RepairShop-Frontend/src/components/TechnicianHeader.vue
+++ RepairShop-Frontend/src/components/TechnicianHeader.vue
@@ -1,14 +1,16 @@
 <template>
     <nav id="header">
-
         <img class ="logo" src="../assets/logo_draft1.png" />
         <div id="container">
 
-            <button id="home" v-on:click="goToTechnicianHomePage()" >Home</button>
+              <button id="present" v-if="homeTab" v-on:click="goToTechnicianHomePage()" >Home</button>
+              <button id="absent" v-if="!homeTab" v-on:click="goToTechnicianHomePage()" >Home</button>
             <span></span>
-            <button id="appointment" v-on:click="goToTechnicianAppointmentPage()">Appointment</button>
+              <button id="present" v-if="appointmentManagerTab" v-on:click="goToTechnicianAppointmentPage()">Appointment</button>
+              <button id="absent" v-if="!appointmentManagerTab" v-on:click="goToTechnicianAppointmentPage()">Appointment</button>
             <span></span>
-            <button id="account" v-on:click="goToTechnicianAccountPage()">My Account</button>
+              <button id="present" v-if="technicianAccountTab" v-on:click="goToTechnicianAccountPage()">My Account</button>
+              <button id="absent" v-if="!technicianAccountTab" v-on:click="goToTechnicianAccountPage()">My Account</button>
             <span></span>
             <button id="logout" v-on:click="logout()">Logout</button>
             <span></span>
@@ -21,6 +23,35 @@ import Router from "../router/index";
 
 export default {
     name: "TechnicianHeader",
+
+    data() {
+          return {
+            homeTab: false,
+            appointmentManagerTab: false,
+            technicianAccountTab: false
+          };
+    },
+
+    created: function(){
+        if(this.$router.history.current.path.localeCompare("/TechnicianHomePage") === 0) {
+            console.log(this.$router.history.current.path);
+            this.homeTab = true;
+            this.appointmentManagerTab = false;
+            this.technicianAccountTab = false;
+        } else if (this.$router.history.current.path.localeCompare("/TechnicianAppointmentPage") === 0) {
+            console.log(this.$router.history.current.path);
+            this.homeTab = false;
+            this.appointmentManagerTab = true;
+            this.technicianAccountTab = false;
+        } else if (this.$router.history.current.path.localeCompare("/TechnicianAccountPage") === 0) {
+            console.log(this.$router.history.current.path);
+            this.homeTab = false;
+            this.appointmentManagerTab = false;
+            this.technicianAccountTab = true;
+        }
+
+    },
+
     methods: {
         goToTechnicianAppointmentPage: function (){
             Router.push({
@@ -70,13 +101,35 @@ export default {
     width: 75px;
     margin: 4px;
 }
-button {
+#absent {
     background-color: Transparent;
     color: white;
     border: none;
+    border-radius: 10px;
 }
-button:hover {
+#absent:hover {
     background-color: firebrick;
+    color: white;
+    border: none;
+    border-radius: 10px;
+}
+#present{
+    background-color: firebrick;
+    color: white;
+    border: none;
+    border-radius: 10px;
+}
+
+#logout{
+    background-color: Transparent;
+    color: white;
+    border: none;
+    border-radius: 10px;
+}
+#logout:hover {
+    background-color: firebrick;
+    color: white;
+    border: none;
     border-radius: 10px;
 }
 
