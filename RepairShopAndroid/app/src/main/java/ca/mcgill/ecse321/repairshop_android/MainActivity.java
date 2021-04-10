90acb8a3a4fd46595be4be917c4bbf465a305111
Saikou Ceesay <ceesaysaikou72@gmail.com>
2021-04-09 22:00:10 -0400

    android folder created

---
+++ RepairShopAndroid/app/src/main/java/ca/mcgill/ecse321/repairshop_android/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.mcgill.ecse321.repairshop_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
