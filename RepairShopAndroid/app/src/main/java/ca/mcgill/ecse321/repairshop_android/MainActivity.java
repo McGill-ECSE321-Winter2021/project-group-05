ec709a76caeaa15325140392a23188e7699dbec4
Yichen Wu <yichen.wu3@mail.mcgill.ca>
2021-04-11 19:27:59 -0400

    sign up UI

--- RepairShopAndroid/app/src/main/java/ca/mcgill/ecse321/repairshop_android/MainActivity.java
+++ RepairShopAndroid/app/src/main/java/ca/mcgill/ecse321/repairshop_android/MainActivity.java
@@ -3,6 +3,7 @@ package ca.mcgill.ecse321.repairshop_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.transition.Scene;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
@@ -42,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void login(View v){
+
         error="";
         final TextView tv_email = (TextView) findViewById(R.id.email);
         final TextView tv_password = (TextView) findViewById(R.id.password);
@@ -51,13 +53,19 @@ public class MainActivity extends AppCompatActivity {
         RadioButton techCheckbox = (RadioButton) findViewById(R.id.techCheckBox);
 
         RequestParams requestParams = new RequestParams();
-        requestParams.put("email",tv_email.toString());
-        requestParams.put("password",tv_password.toString());
+        requestParams.add("email", tv_email.getText().toString());
+        requestParams.add("password",tv_password.getText().toString());
+
+        /* for debugging
+        String x = tv_email.getText().toString();
+        String y = tv_password.getText().toString();
+
+         */
 
         // log in as customer
         if (customerCheckBox.isChecked()){
 
-            HttpUtils.post("/person/customer/login",requestParams, new JsonHttpResponseHandler() {
+            HttpUtils.post("person/customer/login",requestParams, new JsonHttpResponseHandler() {
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                     refreshErrorMessage();
@@ -79,9 +87,10 @@ public class MainActivity extends AppCompatActivity {
         }
         // log in as admin
         else if (adminCheckbox.isChecked()){
-            HttpUtils.post("/person/administrator/login",requestParams, new JsonHttpResponseHandler() {
+            HttpUtils.post("person/administrator/login", requestParams, new JsonHttpResponseHandler() {
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+
                     refreshErrorMessage();
                     tv_email.setText("");
                     tv_password.setText("");
@@ -89,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                     try {
+
                         tv_password.setText("");
                         error += errorResponse.get("message").toString();
                     } catch (JSONException e) {
@@ -102,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
         }
         // log in as tech
         else if (techCheckbox.isChecked()){
-            HttpUtils.post("/person/technician/login",requestParams, new JsonHttpResponseHandler() {
+            HttpUtils.post("person/technician/login",requestParams, new JsonHttpResponseHandler() {
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                     refreshErrorMessage();
@@ -126,6 +136,42 @@ public class MainActivity extends AppCompatActivity {
             error+="You need to select the role!";
             refreshErrorMessage();
         }
+    }
+
+    public void signup(View v){
+        error="";
+        final TextView tv_email = (TextView) findViewById(R.id.email);
+        final TextView tv_password = (TextView) findViewById(R.id.password);
+        final TextView tv_username = (TextView) findViewById(R.id.username);
+
+        String email = tv_email.getText().toString();
+        String username = tv_username.getText().toString();
+        String password = tv_password.getText().toString();
+
+        RequestParams requestParams = new RequestParams();
+        requestParams.add("email", email);
+        requestParams.add("password",password);
+        requestParams.add("username",username);
+
+        HttpUtils.post("person/customer/register/",requestParams, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                refreshErrorMessage();
+                tv_email.setText("");
+                tv_password.setText("");
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    tv_password.setText("");
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+
+        });
 
 
     }
