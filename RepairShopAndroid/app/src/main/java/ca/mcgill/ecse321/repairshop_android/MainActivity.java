62bfcd3c3265ccc4da50cb191c791905e7f7ca9b
Yichen Wu <yichen.wu3@mail.mcgill.ca>
2021-04-10 22:06:55 -0400

    fixed the issues of building

--- RepairShopAndroid/app/src/main/java/ca/mcgill/ecse321/repairshop_android/MainActivity.java
+++ RepairShopAndroid/app/src/main/java/ca/mcgill/ecse321/repairshop_android/MainActivity.java
@@ -3,12 +3,30 @@ package ca.mcgill.ecse321.repairshop_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private String error = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        refreshErrorMessage();
+    }
+
+    private void refreshErrorMessage() {
+        // set the error message
+        TextView tvError = (TextView) findViewById(R.id.error);
+        tvError.setText(error);
+
+        if (error == null || error.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } else {
+            tvError.setVisibility(View.VISIBLE);
+        }
     }
 }
