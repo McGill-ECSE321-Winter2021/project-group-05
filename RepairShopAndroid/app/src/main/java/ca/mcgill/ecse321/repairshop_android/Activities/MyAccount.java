f36773dde1338316b15a6b6b57347a0edc0be985
Yichen Wu <yichen.wu3@mail.mcgill.ca>
2021-04-11 23:02:39 -0400

    commit

---
+++ RepairShopAndroid/app/src/main/java/ca/mcgill/ecse321/repairshop_android/Activities/MyAccount.java
@@ -0,0 +1,16 @@
+package ca.mcgill.ecse321.repairshop_android.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.mcgill.ecse321.repairshop_android.R;
+
+public class MyAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_account);
+    }
+}
