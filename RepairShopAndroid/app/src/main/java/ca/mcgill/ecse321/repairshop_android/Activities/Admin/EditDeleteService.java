b5611a47f22c60cc12e36d58f8d92b075e7bea3a
himelsaha29 <himelsaha29@live.com>
2021-04-12 02:57:16 -0400

    Edit/Delete service page added

---
+++ RepairShopAndroid/app/src/main/java/ca/mcgill/ecse321/repairshop_android/Activities/Admin/EditDeleteService.java
@@ -0,0 +1,16 @@
+package ca.mcgill.ecse321.repairshop_android.Activities.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.mcgill.ecse321.repairshop_android.R;
+
+public class EditDeleteService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_delete_service);
+    }
+}
