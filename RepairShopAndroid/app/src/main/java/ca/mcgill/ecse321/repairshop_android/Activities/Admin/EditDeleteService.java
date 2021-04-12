36782deb42e74d51e3fef7794e5148f62f9edd51
himelsaha29 <himelsaha29@live.com>
2021-04-12 03:14:25 -0400

    Spinner issue fixed

--- RepairShopAndroid/app/src/main/java/ca/mcgill/ecse321/repairshop_android/Activities/Admin/EditDeleteService.java
+++ RepairShopAndroid/app/src/main/java/ca/mcgill/ecse321/repairshop_android/Activities/Admin/EditDeleteService.java
@@ -2,7 +2,19 @@ package ca.mcgill.ecse321.repairshop_android.Activities.Admin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import ca.mcgill.ecse321.repairshop_android.R;
 
@@ -12,5 +24,19 @@ public class EditDeleteService extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_delete_service);
+
+
+
+        // you need to have a list of data that you want the spinner to display
+        List<String> spinnerArray =  new ArrayList<String>();
+        spinnerArray.add("item1");
+        spinnerArray.add("item2");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, spinnerArray);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItems = (Spinner) findViewById(R.id.spinner);
+        sItems.setAdapter(adapter);
     }
 }
