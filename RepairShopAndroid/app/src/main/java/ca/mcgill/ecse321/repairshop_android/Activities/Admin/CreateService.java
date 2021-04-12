02e253852031638e5beadf7888780edc3b46352d
Yichen Wu <yichen.wu3@mail.mcgill.ca>
2021-04-11 22:31:38 -0400

    reorganize the create service

---
+++ RepairShopAndroid/app/src/main/java/ca/mcgill/ecse321/repairshop_android/Activities/Admin/CreateService.java
@@ -0,0 +1,16 @@
+package ca.mcgill.ecse321.repairshop_android.Activities.Admin;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import ca.mcgill.ecse321.repairshop_android.R;
+
+public class CreateService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_service);
+
+    }
+}
