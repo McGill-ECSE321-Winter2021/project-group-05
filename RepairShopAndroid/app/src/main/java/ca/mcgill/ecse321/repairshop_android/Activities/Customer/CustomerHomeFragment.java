3cb54d720bfe6c64ffbdcb366cee757faeefdd22
Saikou Ceesay <ceesaysaikou72@gmail.com>
2021-04-13 01:25:54 -0400

    customer pages updated

--- RepairShopAndroid/app/src/main/java/ca/mcgill/ecse321/repairshop_android/Activities/Customer/CustomerHomeFragment.java
+++ RepairShopAndroid/app/src/main/java/ca/mcgill/ecse321/repairshop_android/Activities/Customer/CustomerHomeFragment.java
@@ -1,17 +1,26 @@
 package ca.mcgill.ecse321.repairshop_android.Activities.Customer;
 
+import android.graphics.Color;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.skyhope.eventcalenderlibrary.CalenderEvent;
+import com.skyhope.eventcalenderlibrary.model.Event;
+
 import ca.mcgill.ecse321.repairshop_android.R;
 
 public class CustomerHomeFragment extends Fragment {
-
+    private CalenderEvent calendarEvent;
 
     public CustomerHomeFragment() {
         // Required empty public constructor
@@ -28,4 +37,25 @@ public class CustomerHomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_customer_home, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setViews(view);
+    }
+
+    private void setViews(View views){
+        calendarEvent = views.findViewById(R.id.calender_event);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void setEvent(){
+        Calendar calendar = Calendar.getInstance();
+        Event event = new Event(calendar.getTimeInMillis(), "Test");
+// to set desire day time milli second in first parameter
+//or you set color for each event
+        Event event2 = new Event(calendar.getTimeInMillis(), "Test", Color.RED);
+        calendarEvent.addEvent(event2);
+    }
+
 }
--- RepairShopAndroid/app/src/main/java/ca/mcgill/ecse321/repairshop_android/Activities/MainActivity.java
+++ RepairShopAndroid/app/src/main/java/ca/mcgill/ecse321/repairshop_android/Activities/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
         // log in as customer
         if (customerCheckBox.isChecked()){
-
+            goToCustomerHomePage();
             HttpUtils.post("person/customer/login",requestParams, new JsonHttpResponseHandler() {
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
@@ -71,7 +71,6 @@ public class MainActivity extends AppCompatActivity {
                     setCurrentCustomer(tv_email.getText().toString());
                     tv_email.setText("");
                     tv_password.setText("");
-                    goToCustomerHomePage();
 
                 }
                 @Override
