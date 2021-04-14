fb7e0851d229a89083db681c5c922c904598d912
himelsaha29 <himelsaha29@live.com>
2021-04-14 00:10:28 -0400

    Dark mode implemented

--- RepairShopAndroid/app/src/main/java/ca/mcgill/ecse321/repairshop_android/Activities/MainActivity.java
+++ RepairShopAndroid/app/src/main/java/ca/mcgill/ecse321/repairshop_android/Activities/MainActivity.java
@@ -1,9 +1,12 @@
 package ca.mcgill.ecse321.repairshop_android.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -253,5 +256,30 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    // create an action bar button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // R.menu.mymenu is a reference to an xml file named mymenu.xml which should be inside your res/menu directory.
+        // If you don't have res/menu, just create a directory named "menu" inside res
+        getMenuInflater().inflate(R.menu.dark_button, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle dark button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.dark_button) {
+            if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
