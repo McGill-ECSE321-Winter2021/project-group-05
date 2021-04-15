9100e5f819a24c5a6e392515881b5abaade23133
Rakshitha Ravi <rakshitharavi18@hotmail.com>
2021-04-15 14:03:39 -0400

    created fragment to assign appointments to technicians

---
+++ RepairShopAndroid/app/src/main/java/ca/mcgill/ecse321/repairshop_android/Activities/Admin/AssignFragment.java
@@ -0,0 +1,125 @@
+package ca.mcgill.ecse321.repairshop_android.Activities.Admin;
+
+// delete service stuff 
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import org.json.JSONException;
+import org.json.JSONObject;
+import ca.mcgill.ecse321.repairshop_android.Activities.Utility.HttpUtils;
+import ca.mcgill.ecse321.repairshop_android.R;
+import cz.msebera.android.httpclient.Header;
+
+public class ServiceFragment extends Fragment {
+
+    private String error = null;
+
+    private Button createServiceButton;
+    private EditText tv_service_name = null;
+    private EditText tv_service_cost = null;
+    private EditText tv_service_duration = null;
+
+    private TextView tvError = null;
+
+    public ServiceFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_service, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        System.out.println("here2");
+        setView(view);
+    }
+
+
+    private void refreshErrorMessage(View view) {
+        if (error == null || error.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } else {
+            tvError.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void setView(View view) {
+        createServiceButton = view.findViewById(R.id.CreateServiceButton);
+        tv_service_name = (EditText) view.findViewById(R.id.newServiceName);
+        tv_service_cost = (EditText) view.findViewById(R.id.newServiceCost);
+        tv_service_duration = (EditText) view.findViewById(R.id.newServiceDuration);
+        tvError = (TextView) view.findViewById(R.id.errorService);
+        createServiceButtonHandler();
+    }
+
+    public void createServiceButtonHandler(){
+        createServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createService(view);
+            }
+        });
+    }
+
+
+    public void createService(final View view){
+
+        error="";
+        RequestParams requestParams = new RequestParams();
+        requestParams.add("newServiceName", tv_service_name.getText().toString());
+        requestParams.add("newServiceCost",tv_service_cost.getText().toString());
+        requestParams.add("newServiceDuration",tv_service_duration.getText().toString());
+
+        Toast.makeText
+                (getActivity(), "INSIDE", Toast.LENGTH_SHORT)
+                .show();
+
+        HttpUtils.post("/bookableService", requestParams, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                refreshErrorMessage(view);
+                tv_service_name.setText("");
+                tv_service_cost.setText("");
+                tv_service_duration.setText("");
+                // Notify with successful message
+                Toast.makeText
+                        (getActivity(), "Service successfully created", Toast.LENGTH_SHORT)
+                        .show();
+
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                    System.out.println("inside try : "+error);
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                    System.out.println("catch : "+error);
+                }
+                refreshErrorMessage(view);
+
+            }
+
+        });
+    }
+}
